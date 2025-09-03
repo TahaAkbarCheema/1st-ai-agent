ee20cce1e5eec0ceb0d22ba5e2e664eac6019dc5
TahaAkbarCheema <tahaakbar.se@gmail.com>
2025-09-04 01:37:28 +0500

    #

--- first_agent/main.py
+++ first_agent/main.py
@@ -1,11 +1,16 @@
-
-from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, set_tracing_disabled, OpenAIChatCompletionsModel
+from agents import (
+    Agent,
+    Runner,
+    AsyncOpenAI,
+    set_default_openai_client,
+    set_tracing_disabled,
+    OpenAIChatCompletionsModel,
+)
 
 api_key = "AIzaSyAFU1Rx_3fnOJ_gbHOhHHfMyvvGYimyycQ"
 
 external_client = AsyncOpenAI(
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-    api_key = api_key
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/", api_key=api_key
 )
 
 set_default_openai_client(external_client)
@@ -13,11 +18,12 @@ set_default_openai_client(external_client)
 set_tracing_disabled(True)
 
 model = OpenAIChatCompletionsModel(
-    model = "gemini-2.5-flash",
-    openai_client = external_client
+    model="gemini-2.5-flash", openai_client=external_client
 )
-agent = Agent(name="HelloAgent", instructions="You are a helpful assistant.", model=model)
+agent = Agent(
+    name="HelloAgent", instructions="You are a helpful assistant.", model=model
+)
 
-result = Runner.run_sync(starting_agent=agent, input="What is AI?")
+result = Runner.run_sync(starting_agent=agent, input="Hello AI Agent")
 
-print(result) 
+print(result)
